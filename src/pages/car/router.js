f0ce39eef76af4e7d4aa84b41bda3c4a2da95cff
ysl <1071267286@qq.com>
2019-01-03 15:36:19 +0800

    完成车

--- src/pages/car/router.js
+++ src/pages/car/router.js
@@ -32,6 +32,89 @@ const router = new Router({
           component: () => import('./views/lineManage/CarLineManage.vue')
         },
         {
+          path: 'carlocation',
+          name: '车辆实时定位',
+          component: () => import('./views/track/CarLocation.vue')
+        },
+        {
+          path: 'tracksearch',
+          name: '轨迹查询',
+          component: () => import('./views/track/TrackSearch.vue')
+        },
+        {
+          path: 'nottravel',
+          name: '三天未出行',
+          component: () => import('./views/track/abnormalsearch/NotTravel.vue')
+        },
+        {
+          path: 'coordinateskewing',
+          name: '坐标偏移',
+          component: () =>
+            import('./views/track/abnormalsearch/CoordinateSkewing.vue')
+        },
+        {
+          path: 'coordinatenomatch',
+          name: '坐标不匹配',
+          component: () =>
+            import('./views/track/abnormalsearch/CoordinateNoMatch.vue')
+        },
+        {
+          path: 'stanceanalyze',
+          name: '落脚点分析',
+          component: () =>
+            import('./views/track/abnormalsearch/CoordinateSkewing.vue')
+        },
+        {
+          path: 'spacesearch',
+          name: '空间查询',
+          component: () => import('./views/track/SpaceSearch.vue')
+        },
+        {
+          path: 'gatherwarn',
+          name: '聚集预警',
+          component: () => import('./views/gather/index.vue')
+        },
+        {
+          path: 'morecar',
+          name: '一证多车',
+          component: () => import('./views/abnormalWarn/MoreCar.vue')
+        },
+        {
+          path: 'moreillegal',
+          name: '违法大户',
+          component: () => import('./views/abnormalWarn/MoreIllegal.vue')
+        },
+        {
+          path: 'testwarn',
+          name: '检验预警',
+          component: () => import('./views/abnormalWarn/TestWarn.vue')
+        },
+        {
+          path: 'overduewarn',
+          name: '审验超期预警',
+          component: () => import('./views/abnormalWarn/OverdueWarn.vue')
+        },
+        {
+          path: 'illegalwarn',
+          name: '违法预警',
+          component: () => import('./views/illegalWarn/IllegalWarn.vue')
+        },
+        {
+          path: 'timewarn',
+          name: '时间预警',
+          component: () => import('./views/illegalWarn/TimeWarn.vue')
+        },
+        {
+          path: 'speedwarn',
+          name: '超速预警',
+          component: () => import('./views/illegalWarn/SpeedWarn.vue')
+        },
+        {
+          path: 'accidentwarn',
+          name: '实时事故预警',
+          component: () => import('./views/accidentWarn/index.vue')
+        },
+        {
           path: 'analyze',
           name: '综合研判分析',
           component: () => import('./views/analyze')
---
+++ src/pages/car/views/track/abnormalsearch/CoordinateNoMatch.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="not-travel">
+    <info-card :height="height">
+      <info-card-item v-for="(item, index) of items"
+                      :key="index">
+        <div class="item-wrap">
+          <span class="item-type">{{item.type}}</span>
+          <p class="item"><span class="item-title">姓名：</span>{{item.name}}</p>
+          <p class="item"><span class="item-title">事故原因：</span>{{item.accidentResult}}</p>
+          <p class="item"><span class="item-title">时间：</span>{{item.time}}</p>
+          <p class="item"><span class="item-title">地点：</span>{{item.site}}</p>
+        </div>
+      </info-card-item>
+    </info-card>
+  </div>
+</template>
+
+<script>
+import { InfoCard, InfoCardItem } from '@/components/InfoCard'
+export default {
+  components: {
+    InfoCard,
+    InfoCardItem
+  },
+  data () {
+    return {
+      height: 'calc(100% - 35px - 20px - 20px)',
+      items: [
+        {
+          name: '张威',
+          type: '非重点人员',
+          accidentResult: '酒驾',
+          time: '2018-11-19 10:21:21',
+          site: '无锡市惠山区金汇路'
+        },
+        {
+          name: '张威',
+          type: '非重点人员',
+          accidentResult: '酒驾',
+          time: '2018-11-19 10:21:21',
+          site: '无锡市惠山区金汇路'
+        },
+        {
+          name: '张威',
+          type: '非重点人员',
+          accidentResult: '酒驾',
+          time: '2018-11-19 10:21:21',
+          site: '无锡市惠山区金汇路'
+        },
+        {
+          name: '张威',
+          type: '非重点人员',
+          accidentResult: '酒驾',
+          time: '2018-11-19 10:21:21',
+          site: '无锡市惠山区金汇路'
+        },
+        {
+          name: '张威',
+          type: '非重点人员',
+          accidentResult: '酒驾',
+          time: '2018-11-19 10:21:21',
+          site: '无锡市惠山区金汇路'
+        },
+        {
+          name: '张威',
+          type: '非重点人员',
+          accidentResult: '酒驾',
+          time: '2018-11-19 10:21:21',
+          site: '无锡市惠山区金汇路'
+        }
+      ]
+    }
+  }
+}
+</script>
